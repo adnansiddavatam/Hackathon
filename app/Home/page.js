129cfd2dcbd38b46cf127a46d359e1332b21d608
Adnan <adnan.ahmed.siddavatam@gmail.com>
2023-12-15 01:20:05 -0700

    message

--- app/Home/page.js
+++ app/Home/page.js
@@ -10,6 +10,7 @@ const HomePage = () => {
   const [confession, setConfession] = useState('');
   const [saitConfessions, setSaitConfessions] = useState([]);
   const [devConfessions, setDevConfessions] = useState([]);
+  const [replyingTo, setReplyingTo] = useState(null); // State to track which confession or reply is being replied to
 
   const handleConfessionChange = (event) => {
     setConfession(event.target.value);
@@ -19,7 +20,8 @@ const HomePage = () => {
     event.preventDefault();
     const newConfession = {
       text: confession,
-      timestamp: new Date().toISOString()
+      timestamp: new Date().toISOString(),
+      replies: [], // Array to store replies
     };
     if (activeTab === 'SAIT') {
       setSaitConfessions([newConfession, ...saitConfessions]);
@@ -29,24 +31,104 @@ const HomePage = () => {
     setConfession('');
   };
 
+  // Helper function to handle replying to a confession or reply
+  const handleReply = (confessionIndex, replyIndex = null) => {
+    const isReplyingToSame = replyingTo?.confessionIndex === confessionIndex && replyingTo?.replyIndex === replyIndex;
+    setReplyingTo(isReplyingToSame ? null : { confessionIndex, replyIndex });
+  };
+
   const currentUser = "SomeUniqueIdentifier"; // Replace with the actual user identifier
 
-  // Helper function to render confession messages
-  const renderConfessionMessage = (conf) => (
-    <div key={conf.timestamp} className="flex items-start mb-4">
-      <div className="mr-4">
+  // Helper function to render confession messages and replies
+  const renderConfessionMessage = (conf, confessionIndex) => (
+    <div key={conf.timestamp} className="flex items-start flex-col mb-4">
+      <div className="mb-2">
         <Link href="/Profile" passHref> {/* Replace "/Profile" with the actual profile page URL */}
           <a>
             <UserAvatar seed={currentUser} /> {/* Use seed instead of username */}
           </a>
         </Link>
       </div>
-      <div>
-        <p className="text-white">{conf.text}</p>
-        <p className="text-gray-400 text-sm">{conf.timestamp}</p>
+      <div className="text-white mb-2">{conf.text}</div>
+      <div className="text-gray-400 text-sm mb-2">{conf.timestamp}</div>
+      <div className="mb-2">
+        {replyingTo?.confessionIndex === confessionIndex && replyingTo?.replyIndex === null ? (
+          <form onSubmit={(event) => handleReplySubmit(event, confessionIndex)}>
+            <textarea
+              value={confession}
+              onChange={(event) => handleConfessionChange(event)}
+              className="w-full p-2 text-black"
+              placeholder="Type your reply here..."
+            />
+            <button type="submit" className="bg-red-600 text-white px-4 py-2">
+              Post Reply
+            </button>
+          </form>
+        ) : (
+          <button onClick={() => handleReply(confessionIndex)} className="text-blue-500">
+            Reply
+          </button>
+        )}
       </div>
+      {conf.replies.map((reply, replyIndex) => (
+        <div key={reply.timestamp} className="bg-gray-800 rounded-md p-2 mt-2">
+          <div className="flex items-start mb-2">
+            <div className="mr-2">
+              <Link href="/Profile" passHref> {/* Replace "/Profile" with the actual profile page URL */}
+                <a>
+                  <UserAvatar seed={currentUser} /> {/* Use seed instead of username */}
+                </a>
+              </Link>
+            </div>
+            <div className="text-white">{reply.text}</div>
+          </div>
+          <div className="text-gray-400 text-sm">{reply.timestamp}</div>
+          <div className="mb-2">
+            {replyingTo?.confessionIndex === confessionIndex && replyingTo?.replyIndex === replyIndex ? (
+              <form onSubmit={(event) => handleReplySubmit(event, confessionIndex, replyIndex)}>
+                <textarea
+                  value={confession}
+                  onChange={(event) => handleConfessionChange(event)}
+                  className="w-full p-2 text-black"
+                  placeholder="Type your reply here..."
+                />
+                <button type="submit" className="bg-red-600 text-white px-4 py-2">
+                  Post Reply
+                </button>
+              </form>
+            ) : (
+              <button onClick={() => handleReply(confessionIndex, replyIndex)} className="text-blue-500">
+                Reply
+              </button>
+            )}
+          </div>
+        </div>
+      ))}
     </div>
   );
+
+  // Helper function to handle submitting a reply
+  const handleReplySubmit = (event, confessionIndex, replyIndex = null) => {
+    event.preventDefault();
+    const newReply = {
+      text: confession,
+      timestamp: new Date().toISOString(),
+      replies: [], // Initialize replies for the new reply
+    };
+    if (replyIndex !== null) {
+      // Replying to a reply
+      const updatedConfessions = [...saitConfessions];
+      updatedConfessions[confessionIndex].replies[replyIndex].replies.push(newReply);
+      setSaitConfessions(updatedConfessions);
+    } else {
+      // Replying to a confession
+      const updatedConfessions = [...saitConfessions];
+      updatedConfessions[confessionIndex].replies.push(newReply);
+      setSaitConfessions(updatedConfessions);
+    }
+    setConfession('');
+    setReplyingTo(null);
+  };
 
   return (
     <div className="bg-black min-h-screen flex flex-col items-center justify-start w-full">
@@ -96,12 +178,12 @@ const HomePage = () => {
         {activeTab === 'SAIT' ? (
           <div>
             <h2 className="text-2xl text-white font-bold mb-4">Campus Confessions</h2>
-            {saitConfessions.map((conf) => renderConfessionMessage(conf))}
+            {saitConfessions.map((conf, index) => renderConfessionMessage(conf, index))}
           </div>
         ) : (
           <div>
             <h2 className="text-2xl text-white font-bold mb-4">Software Development Confessions</h2>
-            {devConfessions.map((conf) => renderConfessionMessage(conf))}
+            {devConfessions.map((conf, index) => renderConfessionMessage(conf, index))}
           </div>
         )}
       </div>
