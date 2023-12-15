5bfeb9d9115189f51023e7bacd5b9138a375308d
Adnan <adnan.ahmed.siddavatam@gmail.com>
2023-12-14 20:09:36 -0700

    tabs

--- app/Home/page.js
+++ app/Home/page.js
@@ -3,59 +3,37 @@ import React, { useState } from 'react';
 
 const HomePage = () => {
   const [activeTab, setActiveTab] = useState('SAIT');
-  const [confession, setConfession] = useState('');
-  const [saitConfessions, setSaitConfessions] = useState([]);
-  const [devConfessions, setDevConfessions] = useState([]);
-
-  const handleConfessionChange = (event) => {
-    setConfession(event.target.value);
-  };
-
-  const handleConfessionSubmit = (event) => {
-    event.preventDefault();
-    if (activeTab === 'SAIT') {
-      setSaitConfessions([...saitConfessions, confession]);
-    } else {
-      setDevConfessions([...devConfessions, confession]);
-    }
-    setConfession('');
-  };
 
   return (
     <div className="bg-black min-h-screen flex flex-col items-center justify-start w-full">
+      {/* Tabs */}
       <div className="flex w-full bg-gray-800">
-        {/* Tabs */}
-        {/* ... existing tab buttons ... */}
+        <button
+          className={`flex-1 py-4 text-lg font-semibold ${activeTab === 'SAIT' ? 'text-red-600 bg-gray-700' : 'text-gray-400'}`}
+          onClick={() => setActiveTab('SAIT')}
+        >
+          SAIT
+        </button>
+        <button
+          className={`flex-1 py-4 text-lg font-semibold ${activeTab === 'Software Development' ? 'text-red-600 bg-gray-700' : 'text-gray-400'}`}
+          onClick={() => setActiveTab('Software Development')}
+        >
+          Software Development
+        </button>
       </div>
-
+      {/* Content */}
       <div className="flex-grow w-full bg-gray-900 p-6">
-        {/* Confession Form */}
-        <form onSubmit={handleConfessionSubmit} className="mb-4">
-          <textarea
-            value={confession}
-            onChange={handleConfessionChange}
-            className="w-full p-2 text-black"
-            placeholder="Type your confession here..."
-          />
-          <button type="submit" className="bg-red-600 text-white px-4 py-2 mt-2">
-            Post Confession
-          </button>
-        </form>
-
-        {/* Content */}
         {activeTab === 'SAIT' ? (
           <div>
+            {/* Content for SAIT tab */}
             <h2 className="text-2xl text-white font-bold mb-4">Campus Confessions</h2>
-            {saitConfessions.map((conf, index) => (
-              <p key={index} className="text-white mb-2">{conf}</p>
-            ))}
+            {/* Components for confessions will go here */}
           </div>
         ) : (
           <div>
+            {/* Content for Software Development tab */}
             <h2 className="text-2xl text-white font-bold mb-4">Software Development Forums</h2>
-            {devConfessions.map((conf, index) => (
-              <p key={index} className="text-white mb-2">{conf}</p>
-            ))}
+            {/* Components for program-specific forums will go here */}
           </div>
         )}
       </div>
