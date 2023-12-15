791f5731ec8ff30b10d874d5bbe08c12bbae6db8
Valentine-Jingwa <119522550+Valentine-Jingwa@users.noreply.github.com>
2023-12-15 05:27:34 -0700

    No Name

--- app/Home/page.js
+++ app/Home/page.js
@@ -1,121 +1,93 @@
-'use client';
-
+"use client";
 import React, { useState, useEffect, useRef } from 'react';
 import Image from 'next/image';
 import UserAvatar from '../Avatar/UserAvatar';
 import Link from 'next/link';
-import { GoogleAuthProvider, onAuthStateChanged, signInWithPopup } from 'firebase/auth';
-import { doc, setDoc, getFirestore, onSnapshot, collection, addDoc, orderBy, query, serverTimestamp } from 'firebase/firestore';
+import { onAuthStateChanged } from 'firebase/auth';
+import { getFirestore, onSnapshot, collection, addDoc, orderBy, query, serverTimestamp } from 'firebase/firestore';
+import { getStorage, ref as storageRef, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { auth, app } from './../_utils/firebase';
 
 const db = getFirestore(app);
+const storage = getStorage(app);
 
 function HomePage() {
-    const [user, setUser] = useState(null); // Update this line accordingly
-    const [messages, setMessages] = useState([]);
-    const [newMessage, setNewMessage] = useState("");
+  const [user, setUser] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [showMenu, setShowMenu] = useState(false);
+  const menuRef = useRef(null);
+  const groups = ['Sait General', 'Software Development Group'];
+  const [filteredGroups, setFilteredGroups] = useState(groups);
 
-
-    const [searchTerm, setSearchTerm] = useState('');
-    const [groups, setGroups] = useState(['Sait General', 'Software Development Group']);
-    const [filteredGroups, setFilteredGroups] = useState(groups);
-    const [showMenu, setShowMenu] = useState(false);
-    const menuRef = useRef(null);
-
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => setUser(user));
+    return unsubscribe;
+  }, []);
 
   useEffect(() => {
     const q = query(collection(db, "messages"), orderBy("timestamp"));
-    const unsubscribe = onSnapshot(q, (snapshot) => {
-      setMessages(snapshot.docs.map((doc) => ({
-        id: doc.id,
-        data: doc.data(),
-      })));
+    const unsubscribe = onSnapshot(q, snapshot => {
+      setMessages(snapshot.docs.map(doc => ({ id: doc.id, data: doc.data() })));
     });
     return unsubscribe;
   }, []);
 
-    // Listen to authentication state changes
-    // useEffect(() => {
-    //     const unsubscribe = auth.onAuthStateChanged((firebaseUser) => {
-    //       if (firebaseUser) {
-    //         // User is signed in
-    //         setUser({
-    //           uid: firebaseUser.uid,
-    //           displayName: firebaseUser.displayName || 'Anonymous',
-    //           photoURL: firebaseUser.photoURL
-    //         });
-    //       } else {
-    //         // User is signed out
-    //         setUser(null);
-    //       }
-    //     });
-    
-    //     return unsubscribe;
-    //   }, []);
-
-    useEffect(() => {
-        onAuthStateChanged(auth, user => {
-          if(user) {
-            setUser(user)
-          } else {
-            setUser(null)
-          }
-        })
-    }, [])
-
-
-
-    const sendMessage = async () => {
-        await addDoc(collection(db, "messages"), {
-          uid: user.uid,
-          photoURL: user.photoURL,
-          displayName: user.displayName,
-          text: newMessage,
-          timestamp: serverTimestamp()
-        })
-    
-        setNewMessage("")
-      }
-
-  // Close the menu when clicked outside
   useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (menuRef.current && !menuRef.current.contains(event.target)) {
-        setShowMenu(false);
-      }
-    };
+    const filtered = searchTerm ? 
+      groups.filter(group => group.toLowerCase().includes(searchTerm.toLowerCase())) : 
+      groups;
+    setFilteredGroups(filtered);
+  }, [searchTerm, groups]);
 
+  const handleFileUpload = async (file) => {
+    const fileRef = storageRef(storage, `messages/${file.name}`);
+    const snapshot = await uploadBytes(fileRef, file);
+    const url = await getDownloadURL(snapshot.ref);
+    return url;
+  };
+
+  const sendMessage = async () => {
+    let imageUrl = null;
+    if (selectedFile) {
+      imageUrl = await handleFileUpload(selectedFile);
+    }
+    
+    await addDoc(collection(db, "messages"), {
+      uid: user.uid,
+      photoURL: user.photoURL,
+      displayName: user.displayName,
+      text: newMessage,
+      image: imageUrl,
+      timestamp: serverTimestamp()
+    });
+
+    setNewMessage("");
+    setSelectedFile(null);
+  };
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setShowMenu(false);
+    }
+  };
+
+  useEffect(() => {
     document.addEventListener('mousedown', handleClickOutside);
     return () => document.removeEventListener('mousedown', handleClickOutside);
   }, []);
 
-
-  //Sort Groups
-  useEffect(() => {
-    if (searchTerm) {
-      setFilteredGroups(groups.filter((group) =>
-        group.toLowerCase().includes(searchTerm.toLowerCase())));
-    } else {
-      setFilteredGroups(groups);
-    }
-  }, [searchTerm, groups]);
-
   return (
     <div className='min-h-screen'>
-      {/* Navbar */}
       <div className='navbar bg-gray-900 px-4 py-2 flex items-center justify-between'>
-        {/* Logo and Menu Items */}
         <div className='flex items-center justify-center' ref={menuRef}>
-          {/* App Image */}
-          <Image
-            src='/S-HELP_Logo.png'
-            alt='App Logo'
-            width={50}
-            height={50}
-            className='cursor-pointer mr-4 rounded-3xl'
-            onClick={() => setShowMenu(!showMenu)}
-          />
-          {/* Menu Items */}
+          <Image src='/S-HELP_Logo.png' alt='App Logo' width={50} height={50} className='cursor-pointer mr-4 rounded-3xl' onClick={() => setShowMenu(!showMenu)} />
           <div className={`flex items-center ${showMenu ? 'visible' : 'invisible'}`}>
             <Link href='/Profile' className='text-white px-4 py-2 hover:bg-gray-700 rounded-md'>Profile</Link>
             <Link href='/Settings' className='text-white px-4 py-2 hover:bg-gray-700 rounded-md'>Settings</Link>
@@ -124,60 +96,36 @@ function HomePage() {
         </div>
       </div>
       <div className='flex min-h-screen'>
-        <div className='resize-x overflow-auto min-w-64 max-w-xs bg-gray-700 overflow-y-auto p-4'>
-            {/* User Avatar and Username */}
+        <div className='resize-x overflow-auto min-w-64 max-w-xs bg-gray-700 p-4'>
+          {user && (
             <div className='flex items-center gap-2 mb-4'>
               <UserAvatar user={user} className='rounded-xl' />
-            {/* <div className='text-white'>{user.displayName}</div> */}
+              <div className='text-white'>{user.displayName || 'Anonymous'}</div>
             </div>
-          {/* Search Bar */}
-          <input
-            type="text"
-            placeholder="Search groups..."
-            className="mb-4 p-2 w-full bg-gray-800 text-white rounded"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          {/* Group List */}
+          )}
+          <input type="text" placeholder="Search groups..." className="mb-4 p-2 w-full bg-gray-800 text-white rounded" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
           {filteredGroups.map((group, index) => (
-            <div key={index} className="text-white py-2 px-4 hover:bg-gray-600 cursor-pointer">
-              {group}
-            </div>
+            <div key={index} className="text-white py-2 px-4 hover:bg-gray-600 cursor-pointer">{group}</div>
           ))}
         </div>
-
-       {/* Main Content */}
-       <div className='flex-grow bg-gray-800 h-[10000px] py-10 px-4 relative'>
-          {/* Messages Display Area */}
+        <div className='flex-grow bg-gray-800 py-10 px-4 relative'>
           <div className='w-11/12 h-full overflow-auto'>
             {messages.map((msg) => (
-              <div key={msg.id} className={`flex items-start mb-4 ${msg.data.uid === user.uid ? 'justify-end' : 'justify-start'}`}>
+              <div key={msg.id} className={`flex items-start mb-4 ${msg.data.uid === user?.uid ? 'justify-end' : 'justify-start'}`}>
                 <UserAvatar user={msg.data} className='w-10 h-10 rounded-full mr-3' />
-                <div className={`rounded px-4 py-2 ${msg.data.uid === user.uid ? 'bg-blue-500 text-white' : 'bg-gray-900 text-gray-300'}`}>
-                  <div className='font-semibold'>{msg.data.displayName}</div>
+                <div className={`rounded px-4 py-2 ${msg.data.uid === user?.uid ? 'bg-blue-500 text-white' : 'bg-gray-900 text-gray-300'}`}>
+                  <div className='font-semibold'></div>
                   <p>{msg.data.text}</p>
                 </div>
               </div>
             ))}
           </div>
-
-          {/* Message Input Area */}
           <div className='fixed bottom-10 w-8/12 mx-auto'>
             <div className='flex gap-2'>
-              <input
-                className='flex-grow p-2 bg-gray-900 text-white rounded'
-                value={newMessage}
-                onChange={(e) => setNewMessage(e.target.value)}
-                placeholder="Type a message..."
-              />
-              <button className='bg-blue-500 hover:bg-blue-600 text-white rounded px-4 py-2' onClick={sendMessage}>
-                Send
-              </button>
+                <input className='flex-grow p-2 bg-gray-900 text-white rounded' value={newMessage} onChange={(e) => setNewMessage(e.target.value)} placeholder="Type a message..." />
+                {/* <input type="file" onChange={handleFileChange} /> */}
+                <button className='bg-blue-500 hover:bg-blue-600 text-white rounded px-4 py-2' onClick={sendMessage}>Send</button>
             </div>
-
-
-
-            
           </div>
         </div>
       </div>
