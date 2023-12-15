49f93e4e8f54dd2e43ab06b67c0446e71e0d9d6f
Adnan <adnan.ahmed.siddavatam@gmail.com>
2023-12-14 19:45:43 -0700

    Styling for First page

--- app/page.js
+++ app/page.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import Link from 'next/link';
 
-function LoginPortal(){
-  return(
-    <div>
-    <Link href="/login">Log in </Link>
-    
-  </div>
-  )
+function LoginPortal() {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen bg-black">
+      {/* Add the logo image above the welcome message */}
+      <img
+        src="/app/images/S-HELP_Logo.png"
+        alt="S-HELP Logo"
+        className="w-48 mb-6" // Adjust the width as needed
+      />
+      <h1 className="text-4xl text-white font-bold mb-4">
+        Welcome to S-HELP
+      </h1>
+      <Link className="text-white bg-red-600 hover:bg-red-700 text-lg font-semibold px-6 py-3 rounded transition duration-300" href="/login">
+          Log in
+      </Link>
+    </div>
+  );
 }
 
-export default LoginPortal
+export default LoginPortal;
